98c3a1e14185e861e9af58f5be34f47929b44107
013thedaniel <dzuban.daniil@gmail.com>
2024-06-23 15:33:06 +0300

    practice

--- user.py
+++ user.py
@@ -1,3 +1,9 @@
+# Програма, що запитує у користувача його ім'я, вік та місце проживання.
+# Після цього виводить наступні три рядки:
+# "This is <ім'я>"
+# "It is <вік>"
+# "S(he) lives in <місце проживання>"
+
 name = input("What is your name? ")
 age = input("How old are you? ")
 residence = input("Where do you live? ")
