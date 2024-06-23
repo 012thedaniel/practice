624e776317e10dbba2eb0e854967f6e920ae290d
013thedaniel <dzuban.daniil@gmail.com>
2024-06-23 15:22:29 +0300

    practice

--- types_of_variables.py
+++ types_of_variables.py
@@ -1,22 +1,23 @@
-# 1. Ініціалізація змінних
+# 1. Змінній varInt надається значення 10, varFloat - значення 8.4, var_str - 'No'.
 varInt = 10
 varFloat = 8.4
 varStr = "No"
 
-# 2. Збільшення varInt в 3.5 рази і прив'язка до varBig
+# 2. Значення, що зберігається в змінній varInt, збільшено в 3.5 рази та зв'язано зі змінною varBig.
 varBig = varInt * 3.5
 
-# 3. Зменшення varFloat на одиницю
+# 3. Значення, збережене в змінній varFloat, зменшено на одиницю та зв'язано з тією ж змінною.
 varFloat -= 1
 
-# 4. Розподіл varInt на varFloat і varBig на varFloat без прив'язки до змінних
+# 4. varInt розділено на varFloat і varBig на varFloat без прив'язки до жодної зі змінних
 varInt / varFloat
 varBig / varFloat
 
-# 5. Зміна значення varStr на "NoNoYesYesYes"
+# 5. Значення, збережене в змінній varStr, змінено на "NoNoYesYesYes"
+# *з використанням операції конкатенації (+) і повторення рядка (*)
 varStr = "No" * 2 + "Yes" * 3
 
-# 6. Вивід значень всіх змінних
+# Вивід значень всіх змінних
 print(f"varInt: {varInt}")
 print(f"varFloat: {varFloat}")
 print(f"varBig: {varBig}")
